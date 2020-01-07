179f663ab5e9056afc2baf68de1f55355cb8721e
miia <miia@com.tw>
2020-01-07 13:56:27 +0800

    1st

--- app/src/main/java/com/fju/member/AgeActivity.java
+++ app/src/main/java/com/fju/member/AgeActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class AgeActivity extends AppCompatActivity {
 
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
